0ae9b22d3609b1b2cdf2a78ae19d929e6b0c5d0e
Nawaritta <Nor.benhaddou@gmail.com>
2023-06-06 22:21:08 +0000

    task 12 fixed

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -7,5 +7,5 @@ def fizzbuzz():
                 print(" ", end='')
         if i % 5 == 0:
             print("Buzz ", end='')
-        if i % 15 != 0:
+        if i % 5 != 0 and i % 3 != 0:
             print(f"{i:d} ", end='')
