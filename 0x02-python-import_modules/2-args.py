b7f183e9c47b090366616bdfce12092685b17252
Nawaritta <Nor.benhaddou@gmail.com>
2023-06-08 17:59:12 +0000

    mandatory tasks

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    args = len(argv) - 1
+
+    print("{} argument".format(args), end='')
+
+    if args == 0:
+        print("s.")
+    elif args == 1:
+        print(":")
+    else:
+        print("s:")
+
+    for i in range(args):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
