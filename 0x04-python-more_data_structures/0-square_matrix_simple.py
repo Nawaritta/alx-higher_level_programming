07d2608f9a496a30c8415a4f5aa454318c90eb54
Nawaritta <Nor.benhaddou@gmail.com>
2023-06-15 02:00:41 +0000

    non empty readme file

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    if(matrix):
-        return ([[y*y for y in x] for x in matrix])
+    if matrix:
+        square_matrix = []
+        for i in matrix:
+            lis = []
+            for j in i:
+                lis += [j*j, ]
+            square_matrix += [lis, ]
+        return square_matrix
