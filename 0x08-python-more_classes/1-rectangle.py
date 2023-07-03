5552043a4d28acfbcdd2fda4dcb0b0b2e11f4a29
Nawaritta <Nor.benhaddou@gmail.com>
2023-07-03 21:59:15 +0000

    class documentation

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -8,6 +8,7 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         """
         Initialize a new instance of Rectangle
+
         args:
         width(int): the width of the rectangle
         height(int): the height of the rectangle
