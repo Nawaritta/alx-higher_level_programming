ee83dca76cef65a596666e75b1314911ad6e08bd
Nawaritta <Nor.benhaddou@gmail.com>
2023-07-06 01:14:16 +0000

    infinit divider replaced by 10

--- 0x07-python-test_driven_development/2m.py
+++ 0x07-python-test_driven_development/2m.py
@@ -4,8 +4,8 @@
 matrix_divided = __import__('2-matrix_divided').matrix_divided
 
 try:
-    matrix = [[3, "9"], [12, 3]]
-    print(matrix_divided(matrix, 2))
+    matrix = [[10, 20, 30]]
+    print(matrix_divided(matrix, float('inf')))
     print(matrix)
 except Exception as e:
     print(e)
