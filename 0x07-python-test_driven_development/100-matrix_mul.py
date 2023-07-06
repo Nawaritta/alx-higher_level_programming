c5976facff91c29f30d875dbb68ba7a2a30ac7a2
Nawaritta <Nor.benhaddou@gmail.com>
2023-07-06 22:11:51 +0000

    multiplication matrices

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -34,28 +34,16 @@ def matrix_mul(m_a, m_b):
         raise TypeError("m_b should contain only integers or floats")
 
     if any(len(row) != len(m_a[0]) for row in m_a):
-        raise TypeError("each row of m_a must should be of the same size")
+        raise TypeError("each row of m_a must be of the same size")
     if any(len(row) != len(m_b[0]) for row in m_b):
-        raise TypeError("each row of m_b must should be of the same size")
+        raise TypeError("each row of m_b must be of the same size")
 
     if len(m_a[0]) != len(m_b):
         raise ValueError("m_a and m_b can't be multiplied")
 
-    invers_m_b = []
-    for c in range(len(m_b[0])):
-        invers_col = []
-        for r in range(len(m_b)):
-            invers_col.append(m_b[r][c])
-        invers_m_b.append(invers_col)
-
-    mul_matrix = []
-    for row in m_a:
-        invers_row = []
-        for col in invers_m_b:
-            x = 0
-            for i in range(len(invers_m_b[0])):
-                x += row[i] * col[i]
-            invers_row.append(x)
-        mul_matrix.append(invers_row)
-
+    mul_matrix = [[0] * len(m_b[0]) for _ in range(len(m_a))]
+    for i in range(len(m_a)):
+        for j in range(len(m_b[0])):
+            for k in range(len(m_b)):
+                mul_matrix[i][j] += m_a[i][k] * m_b[k][j]
     return mul_matrix
