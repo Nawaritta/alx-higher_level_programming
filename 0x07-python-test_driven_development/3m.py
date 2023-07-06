ee07c379241a57e91c2a2fcdee2b742c751fdca8
Nawaritta <Nor.benhaddou@gmail.com>
2023-07-06 02:15:21 +0000

    only the first name is given

--- 0x07-python-test_driven_development/3m.py
+++ 0x07-python-test_driven_development/3m.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
+""" Doc """
+
 say_my_name = __import__('3-say_my_name').say_my_name
 
-say_my_name("John", "Smith")
-say_my_name("Walter", "White")
-say_my_name("Bob")
-say_my_name()
 try:
-    say_my_name(12, "White")
+    say_my_name("Alice")
+
 except Exception as e:
     print(e)
