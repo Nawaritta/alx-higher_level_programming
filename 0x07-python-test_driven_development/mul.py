7ed608d0c9638c0279f42b345b8ecad64d85836b
Nawaritta <Nor.benhaddou@gmail.com>
2023-07-08 01:49:09 +0000

    matrix multiplication all required test are added

--- 0x07-python-test_driven_development/mul.py
+++ 0x07-python-test_driven_development/mul.py
@@ -1,5 +1,27 @@
 #!/usr/bin/python3
 matrix_mul = __import__('100-matrix_mul').matrix_mul
 
-print(matrix_mul([[1, 2], [3, 4]], [[1, 2], [3, 4]]))
-print(matrix_mul([[1, 2]], [[3, 4], [5, 6]]))
+
+try:
+    print(matrix_mul([[1, 2], [3, 4]], [[1, 2], [3, 4]]))
+
+except Exception as e:
+    print(e)
+    print(type(e))
+
+
+
+
+try:
+    print(matrix_mul([[1, 2], [3, 4]]))
+
+except Exception as e:
+    print(e)
+    print(type(e))
+
+try:
+    print(matrix_mul())
+
+except Exception as e:
+    print(e)
+    print(type(e))
