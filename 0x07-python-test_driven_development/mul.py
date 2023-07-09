afd8859ed9f49e728082bca7e8fa4f30c3b36f14
Nawaritta <Nor.benhaddou@gmail.com>
2023-07-09 03:05:45 +0000

    dot replaced with matmul and error message corrected

--- 0x07-python-test_driven_development/mul.py
+++ 0x07-python-test_driven_development/mul.py
@@ -1,27 +1,33 @@
 #!/usr/bin/python3
+""" Doc """
 lazy_matrix_mul = __import__('101-lazy_matrix_mul').lazy_matrix_mul
 
-
+print("Failed 1")
+m_a = "m_a is a string"
+m_b = [[1, 2],[3, 8]]
 try:
-    print(matrix_mul([[1, 2], [3, 4]], [[1, 2], [3, 4]]))
+    print(lazy_matrix_mul(m_a, m_b))
+except Exception as e:
+    print(e)
+    print(type(e))
 
+print("Failed 2 ")
+m_a = [[1, 2],[3, 8]]
+m_b = "m_b is a string"
+try:
+    print(lazy_matrix_mul(m_a, m_b))
 except Exception as e:
     print(e)
     print(type(e))
 
 
+print("\n ma = Holberton / mb = [[5, 6], [7, 8]]")
 
 
+m_a = "Holberton"
+m_b = [[5, 6], [7, 8]]
 try:
-    print(matrix_mul()
-
-except Exception as e:
-    print(e)
-    print(type(e))
-
-try:
-    print(matrix_mul())
-
+    print(lazy_matrix_mul(m_a, m_b))
 except Exception as e:
     print(e)
     print(type(e))
---
+++ 0x07-python-test_driven_development/nnmp.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+import numpy as np
+
+
+print("no args")
+
+
+
+matrix1 = [[1, 2], [3, 4]]
+matrix2 = [[5, 6], [7, 8], [9, 10]]
+
+try:
+    result = np.matmul()
+except Exception as e:
+     print(e)
+     print(type(e))
+
+
+print("\n  140")
+
+matrix1 = [[1, 2],[3, 4]]
+matrix2 = [[5, 6], [7, 8], [9, 10]]
+
+try:
+    result = np.matmul(matrix1, matrix2)
+except Exception as e:
+    print(e)
+    print(type(e))
