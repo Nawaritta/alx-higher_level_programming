f1dc00b07865d805212bb8c0328390f876ab5f59
Nawaritta <Nor.benhaddou@gmail.com>
2023-07-11 22:07:17 +0000

    class student

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,2 +1,24 @@
 #!/usr/bin/python3
 """Python - Input/Output"""
+
+
+class Student:
+    """class Student that defines a student by"""
+
+    def __init__(self, first_name, last_name, age):
+        """student constractor"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieves a dictionary representation of a Student instance.
+
+        Returns:
+            dict: Dictionary representation of the Student instance.
+
+        """
+        if (isinstance(attrs, list) and all(isinstance(items, str) for items in attrs)):
+            return {x: getattr(self, x) for x in attrs if hasattr(self, x)}
+        return self.__dict__
--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,2 +1,22 @@
 #!/usr/bin/python3
 """Python - Input/Output"""
+
+
+class Student:
+    """class Student that defines a student by"""
+
+    def __init__(self, first_name, last_name, age):
+        """student constractor"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Retrieves a dictionary representation of a Student instance.
+
+        Returns:
+            dict: Dictionary representation of the Student instance.
+
+        """
+        return self.__dict__
