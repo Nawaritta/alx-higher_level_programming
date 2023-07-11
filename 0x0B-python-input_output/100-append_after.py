db4be16f8c41a98348c8101fc3c1edb7f0c7aa01
Nawaritta <Nor.benhaddou@gmail.com>
2023-07-11 21:35:20 +0000

    advanced tasks check

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,2 +1,21 @@
 #!/usr/bin/python3
 """Python - Input/Output"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """
+    inserts a line of text to a file, after each line
+    containing a specific string
+
+    Args:
+    search_string (str):
+    new_string (str):
+    """
+    with open(filename, "r", encoding="utf-8") as f:
+        text = f.readlines()
+
+    with open(filename, "w", encoding="utf-8") as f:
+        for line in text:
+            f.write(line)
+            if search_string in line:
+                f.write(new_string)
