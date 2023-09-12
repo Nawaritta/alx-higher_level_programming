f21977b12316c13530e27cbd586a4585ee9db0b3
Nawaritta <Nor.benhaddou@gmail.com>
2023-09-12 02:26:57 +0000

    --

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,14 +2,16 @@
 const argv = process.argv;
 
 if (argv.length < 4) {
-  console.log('0');
+    console.log('0');
 } else {
-  let biggest = Number(argv[2]);
-    let sec = Number(argv[2]);
-    let i = 2;
-    for (;i < argv.length; i++) { if ( Number(argv[i]) > biggest) { biggest = Number(argv[i]);}}
-    i=2;
-    for (;i < argv.length; i++) { if ( Number(argv[i]) > sec && Number(argv[i]) != biggest) { sec = Number(argv[i]);}}
-
-  console.log(sec);
+    let biggest = Number(argv[2]);
+    let sec = Number(argv[3]);
+    let i = 3;
+    for (;i < argv.length; i++) {
+	if (Number(argv[i]) > biggest) {
+	    sec = biggest;
+	    biggest = Number(argv[i]);
+	}
+    }
+    console.log(sec);
 }
