30874b138e21384087dc529d4fa347ae9c346802
Nawaritta <Nor.benhaddou@gmail.com>
2023-09-17 01:53:23 +0000

    task 13: the coding style fixed

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -20,5 +20,5 @@ if __name__ == "__main__":
 
     for state in session.query(State).order_by(State.id):
         if "a" in state.name:
-             session.delete(state)
-             session.commit()
+            session.delete(state)
+            session.commit()
