6327338351c1959a6464aeb8954e8fc7f416ec89
Nawaritta <Nor.benhaddou@gmail.com>
2023-09-18 00:04:13 +0000

    advanced tasks

---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+contains the class definition of a State and an instance
+City relationship
+"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+
+class State(Base):
+    """This class inherits from Base Tips
+    This class attributes represents a relationship with the class City.
+   """
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="all, delete")
