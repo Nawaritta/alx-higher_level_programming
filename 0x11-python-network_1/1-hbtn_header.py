4968f8fcdbca7b7edd7b49bbebccd8a229d50f96
Nawaritta <Nor.benhaddou@gmail.com>
2023-09-29 14:30:25 -0700

    the coding style

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Python script that takes in a URL, sends a request to the URL and displays 
+Python script that takes in a URL, sends a request to the URL and displays
 the value of the X-Request-Id variable found in the header of the response.
 """
 import urllib.request
@@ -12,4 +12,3 @@ if __name__ == "__main__":
     with urllib.request.urlopen(url) as response:
         x_request_id = response.headers.get("X-Request-Id")
     print(x_request_id)
-   
--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Python script that takes in a URL, sends a request to the URL and 
+Python script that takes in a URL, sends a request to the URL and
 displays the value of the variable X-Request-Id in the response header
 """
 
@@ -8,7 +8,7 @@ import requests
 import sys
 
 if __name__ == '__main__':
-    
+
     url = sys.argv[1]
     response = requests.get(url)
     x_request_id = response.headers['X-Request-Id']
